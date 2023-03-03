d9929adaa70306dd040ee2462a53835899ee21c7
Jarod Wilson <jaroddwilson2000@gmail.com>
2023-03-02 19:38:29 -0800

    fixed issue with runtime args

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import { getDatabase, ref, child, get } from "firebase/database";
 
 function App() {
+  /*
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  movieTitle = "" // Movie of the Day
+  */
   return (
     <div className="App">
       <header className="App-header">
