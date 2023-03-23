4308975c10d81f177f3b52923053e61e8add8d09
Jarod Wilson <jaroddwilson2000@gmail.com>
2023-03-23 01:51:47 -0700

    Changed MovieBanner component to use CardMedia

--- src/components/MovieBanner.js
+++ src/components/MovieBanner.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 
 // Consider using CardMedia for background image
 
 function MovieBanner (props) {
     return <div style={{marginTop: '2%', marginLeft: '10%', width: '80%'}}>
-        <Card
+        <CardMedia
             sx={{
                 width: '100%',
-                height: 100
+                height: 100,
+                ':hover': {
+                    boxShadow: 20, // theme.shadows[20]
+                }
             }}
-        >{props.title}</Card>
+            image="https://img.freepik.com/free-vector/online-cinema-banner-with-open-clapper-board-film-strip_1419-2242.jpg"
+        >
+            <p style={{paddingTop: 35, paddingLeft: 10, size: 20}}>{props.title}</p>
+        </CardMedia>
     </div>;
 }
 
