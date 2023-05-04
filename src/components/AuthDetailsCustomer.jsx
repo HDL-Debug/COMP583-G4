ade41b3419deb5438d2cba4ff6bd3fb860775cdc
HDL-Debug <109693433+HDL-Debug@users.noreply.github.com>
2023-05-04 15:50:00 -0700

    Working Protected Routing with minor page redesign found bug with long authentication times

--- src/components/AuthDetailsCustomer.jsx
+++ src/components/AuthDetailsCustomer.jsx
@@ -92,26 +92,59 @@ const AuthDetailsCustomer = () => {
 
   return (
     <>
-      <NavbarCustomer />
-      <div style={{ backgroundColor: 'white', width: 'fit-content', margin: 'auto', padding: '20px', borderRadius: '5px' }}>
-        <h1 className='text-center text-3xl font-bold'>Account Information</h1>
-        {authUser ? (
-          <>
-            <p>{`Currently logged in user: ${authUser.email}`}</p>
-            <p>{`User role: ${userRole}`}</p>
-            <div>
-              <label htmlFor="newEmail">New Email:</label>
-              <input type="email" id="newEmail" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
-              <button onClick={updateEmailHandler}>Update Email</button>
-            </div>
-            <button onClick={resetPassword}>Reset Password</button>
-            <button onClick={userSignOut}>Sign Out</button>
-          </>
-        ) : (
-          <p>Signed Out</p>
-        )}
-      </div>
-    </>
+  <NavbarCustomer />
+  <div style={{ backgroundColor: 'white', width: 'fit-content', margin: 'auto', padding: '20px', borderRadius: '5px' }}>
+    <h1 className='text-center text-3xl font-bold'>Account Information</h1>
+    {authUser ? (
+      <>
+        <p>{`Currently logged in user: ${authUser.email}`}</p>
+        <p>{`User role: ${userRole}`}</p>
+        <div>
+          <label htmlFor="newEmail">New Email:</label>
+          <input type="email" id="newEmail" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+          <button className="update-btn" onClick={updateEmailHandler}>Update Email</button>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <button className="reset-btn" style={{ marginRight: '10px' }} onClick={resetPassword}>Reset Password</button>
+          <button className="sign-out-btn" onClick={userSignOut}>Sign Out</button>
+        </div>
+      </>
+    ) : (
+      <p>Signed Out</p>
+    )}
+  </div>
+  <style>
+    {`
+      .update-btn, .reset-btn, .sign-out-btn {
+        background-color: #fff;
+        border: 2px solid #000;
+        color: #000;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 5px;
+        transition: background-color 0.3s ease, color 0.3s ease;
+      }
+      .update-btn:hover, .reset-btn:hover, .sign-out-btn:hover {
+        background-color: #000;
+        color: #fff;
+      }
+      .reset-btn {
+        margin-right: 10px;
+      }
+      div[style] {
+        width: 100%;
+        text-align: center;
+      }
+    `}
+  </style>
+</>
+
+
   );
 };
 
