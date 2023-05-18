548f2c59e55069c7195517c9754dd62de51a9352
HDL <hunter.lakatta.654@my.csun.edu>
2023-05-17 17:03:58 -0700

    Final View

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,7 +1,7 @@
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth, firestore } from '../firebase'; // Import Firebase SDK modules
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import { findUser } from "../assets/Utils";
 
 const Customers = () => {
@@ -9,16 +9,19 @@ const Customers = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const firestore = getFirestore();
 
   const fetchCustomers = async () => {
     setIsLoading(true);
     try {
-      const customersRef = firestore.collection("users");
-      const snapshot = await customersRef.get();
-      const customers = snapshot.docs.map((doc) => ({
+      const customersRef = collection(firestore, "users");
+      const querySnapshot = await getDocs(customersRef);
+      let customers = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
+
+      customers = customers.filter(e => !(e.role === "admin"));
       setCustomers(customers);
       setIsLoading(false);
     } catch (error) {
@@ -27,19 +30,19 @@ const Customers = () => {
     }
   };
 
-
   const refreshTable = async () => {
     setIsLoading(true);
     try {
-      const customersRef = firestore.collection("users");
-      const snapshot = await customersRef.get();
-      const customers = snapshot.docs.map((doc) => ({
+      const customersRef = collection(firestore, "users");
+      const querySnapshot = await getDocs(customersRef);
+      const customers = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      const updatedCustomers = customers.filter((customer) => {
+      let updatedCustomers = customers.filter((customer) => {
         return findUser(firestore, customer.uid);
       });
+      updatedCustomers = updatedCustomers.filter(e => !(e.role === "admin"));
       setCustomers(updatedCustomers);
       setIsLoading(false);
     } catch (error) {
@@ -55,13 +58,13 @@ const Customers = () => {
   return (
     <>
       <Navbar />
-      <div style={{border: "1px solid #ddd", padding: "10px"}}>
+      <div style={{ border: "1px solid #ddd", padding: "10px" }}>
         {isLoading ? (
           <div>Loading...</div>
         ) : error ? (
           <div>Error: {error.message}</div>
         ) : (
-          <table style={{width: "100%", backgroundColor: "white"}}>
+          <table style={{ width: "100%", backgroundColor: "white" }}>
             <thead>
               <tr>
                 <th>Email</th>
@@ -70,18 +73,20 @@ const Customers = () => {
               </tr>
             </thead>
             <tbody>
-              {customers.map((users) => (
-                <tr key={users.id}>
-                  <td>{users.email}</td>
-                  <td>{users.role}</td>
-                  <td>{users.lastlogin}</td>
+              {customers.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>{user.lastLogin}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         )}
       </div>
-      <button style={{marginTop: "10px"}} onClick={refreshTable}>Refresh Table</button>
+      <button style={{ marginTop: "10px" }} onClick={refreshTable}>
+        Refresh Table
+      </button>
     </>
   );
 };
